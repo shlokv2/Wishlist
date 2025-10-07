701b11706950fc7515c990cf7babe742746c3dc4
Shlok Khandelwal <trickmasterlordshlok@gmail.com>
2025-10-07 07:07:28 +0530

    7 oct Sorting1 DSA completed

--- DSA/lec_14_selectionsort.cpp
+++ DSA/lec_14_selectionsort.cpp
@@ -14,6 +14,45 @@ void selection_sort(int arr[], int n){
         arr[i] = temp;
     }
 }
+void bubble_sort(int arr[], int n){
+    for(int i=n-1; i<=1;i++){
+        for(int j=0; j<=i-1; j++){
+            if(arr[j]>arr[j+1]){
+            int temp=arr[j+1];
+            arr[j+1]=arr[j];
+            arr[j]= temp;
+            }
+        }
+    }
+}
+void bubble_sort_optimized(int arr[], int n){
+    for(int i=n-1; i<=1;i++){
+        int didSwap=0;
+        for(int j=0; j<=i-1; j++){
+            if(arr[j]>arr[j+1]){
+            int temp=arr[j+1];
+            arr[j+1]=arr[j];
+            arr[j]= temp;
+            didSwap=1;
+            }
+        }
+        if(didSwap=0){
+            break;
+        }
+        cout<<"runs\n"; //to check how many times it runs
+    }
+}
+void insertion_sort(int arr[], int n) {
+    for(int i = 0; i <= n-1; i++) {       
+        int j = i;                       // j points to current element
+        while(j > 0 && arr[j-1] > arr[j]) {
+            int temp = arr[j-1];         // swap arr[j-1] and arr[j]
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+            j--;                         // move left to continue checking
+        }
+    }
+}
 
 int main(){
     int n;
@@ -25,9 +64,9 @@ int main(){
         cin >> arr[i];
     }
 
-    selection_sort(arr, n);
+    bubble_sort(arr, n);
 
-    cout << "Sorted array using selection sort is: ";
+    cout << "Sorted array using bubble sort is: ";
     for(int i = 0; i < n; i++){
         cout << arr[i] << " ";
     }
